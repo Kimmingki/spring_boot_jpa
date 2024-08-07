7c4ef5372d161ac38fa8c7811be505880c7ced91
Kimmingki <rlaalsrl9569@naver.com>
2024-08-07 21:58:19 +0900

    상품 서비스 개발

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,7 +1,43 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 아이템 등록
+     * @param item
+     */
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    /**
+     * 전체 아이템 목록 조회
+     * @return
+     */
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    /**
+     * 개별 아이템 조회
+     * @param itemId
+     * @return
+     */
+    public Item findItem(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
 }
